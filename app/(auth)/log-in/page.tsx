038740a7b053e66f1afae7de4cdb1054cfde7fcb
adnan <sirgeant10@gmail.com>
2025-06-01 11:46:37 +0600

    Add link.

--- app/(auth)/log-in/page.tsx
+++ app/(auth)/log-in/page.tsx
@@ -2,18 +2,14 @@
 import Link from "next/link";
 import { useState } from "react";
 import image from "@/assets/form/image.png";
-
-
 import { toast } from "react-toastify";
 import Image from "next/image";
 
 const Form = () => {
- const [form2Data, setForm2Data] = useState({
-  email: "",
-  password: "",
-  
-});
-  
+  const [form2Data, setForm2Data] = useState({
+    email: "",
+    password: "",
+  });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -42,15 +38,15 @@ const Form = () => {
     <div className="container mx-auto py-8 px-10 max-w-7xl flex flex-col items-center animate-fade-in">
       {/* Logo */}
       <div className="text-center mb-4 lg:mb-8">
-          <Link href="/">
-                  <Image
-                    src={image}
-                    alt="GoldFish Ladder"
-                    width={876}
-                    height={400}
-                    className=" h-auto mx-auto mb-4 animate-slide-down"
-                  />
-                </Link>
+        <Link href="/">
+          <Image
+            src={image}
+            alt="GoldFish Ladder"
+            width={876}
+            height={400}
+            className=" h-auto mx-auto mb-4 animate-slide-down"
+          />
+        </Link>
       </div>
 
       {/* Form Container */}
@@ -64,8 +60,6 @@ const Form = () => {
             {/* Grid layout for form fields */}
             <div className=" flex flex-col items-center  w-full max-w-[800px]">
               <div className="grid md:mx-[15px] mx-[0] grid-cols-3 gap-y-14 gap-x-6 w-[90%] w-max-3xl">
-
-
                 {/* EMAIL field */}
                 <label
                   htmlFor="email"
@@ -85,8 +79,8 @@ const Form = () => {
                   />
                 </div>
 
-                 {/* PASSWORD field */}
-                 <label
+                {/* PASSWORD field */}
+                <label
                   htmlFor="password"
                   className="col-span-1 flex items-center md:text-[25px] text-[17px] font-[400] font-bebas"
                 >
@@ -105,50 +99,41 @@ const Form = () => {
                 </div>
               </div>
 
-
               {/* register button */}
               <div className="mt-8 w-full">
-              <div className="text-center">
-                <button
-                  onClick={handleSubmit}
-                  className="w-[91%] bg-[#FFB32C] text-white text-5xl font-medium font-bebas py-2 lg:py-3 rounded-2xl shadow-lg  hover:bg-[#ffd07f] transition-colors mb-7 mt-11 "
+                <div className="text-center">
+                  <button
+                    onClick={handleSubmit}
+                    className="w-[91%] bg-[#FFB32C] text-white text-5xl font-medium font-bebas py-2 lg:py-3 rounded-2xl shadow-lg  hover:bg-[#ffd07f] transition-colors mb-7 mt-11 "
+                  >
+                    LOGIN
+                  </button>
+                </div>
+              </div>
+
+              <div className="flex  w-full justify-center items-center">
+                <div className="w-[40%] border-[1px] border-black h-0.5  "></div>
+                <p className="text-xl font-bebas md:text-2xl lg:text-3xl font-medium mx-[3%]">
+                  OR
+                </p>
+                <div className="w-[40%] border-[1px] border-black  "></div>
+              </div>
+
+              {/* Sign up with Selection */}
+
+              <div className="w-full flex flex-col  items-center">
+                <h2 className="text-xl font-bebas md:text-2xl lg:text-3xl font-medium mb-5 mt-6">
+                  Don&apos;t have an account?
+                </h2>
+
+                <Link
+                  href={"/sign-in"}
+                  className="text-lg font-bebas md:text-xl lg:text-2xl font-medium mb-12 text-[#854836]"
                 >
-                  LOGIN
-                </button>
+                  Sign Up
+                </Link>
               </div>
             </div>
-
-
-            <div className="flex  w-full justify-center items-center">
-                <div className="w-[40%] border-[1px] border-black h-0.5  "></div>
-                <p className="text-xl font-bebas md:text-2xl lg:text-3xl font-medium mx-[3%]">OR</p>
-                <div className="w-[40%] border-[1px] border-black  "></div>
-
-            </div>
-
-
-
-
-                {/* Sign up with Selection */}
-
-                <div className="w-full flex flex-col  items-center">
-                  <h2 className="text-xl font-bebas md:text-2xl lg:text-3xl font-medium mb-5 mt-6">
-                  Don&apos;t have an account?
-                  </h2>
-                 
-                  <p className="text-lg font-bebas md:text-xl lg:text-2xl font-medium mb-12 text-[#854836]">login</p>
-                  
-                </div>
-            </div>
-
-
-
-
-
-
-
-
-
           </div>
         </div>
       </div>
--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -2,25 +2,20 @@
 import Link from "next/link";
 import { useState } from "react";
 import image from "@/assets/form/image.png";
-import insta from "@/assets/icons/Insta icon.png";
 import google from "@/assets/icons/google Icon.png";
-import lindedin from "@/assets/icons/in Icons.png";
-
-
 import { toast } from "react-toastify";
 import Image from "next/image";
 
 const Form = () => {
- const [formData, setFormData] = useState({
-  name: "",
-  email: "",
-  password: "",
-  confirmPassword: "",
-  role: "student", // Default value
-  rememberMe: false, // New field for "Remember Me"
-  acceptTerms: false, // New field for "Accept Terms"
-});
-  
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    role: "student", // Default value
+    rememberMe: false, // New field for "Remember Me"
+    acceptTerms: false, // New field for "Accept Terms"
+  });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -49,15 +44,15 @@ const Form = () => {
     <div className="container mx-auto py-8 px-10 max-w-7xl flex flex-col items-center animate-fade-in">
       {/* Logo */}
       <div className="text-center mb-4 lg:mb-8">
-          <Link href="/">
-                  <Image
-                    src={image}
-                    alt="GoldFish Ladder"
-                    width={876}
-                    height={400}
-                    className=" h-auto mx-auto mb-4 animate-slide-down"
-                  />
-                </Link>
+        <Link href="/">
+          <Image
+            src={image}
+            alt="GoldFish Ladder"
+            width={876}
+            height={400}
+            className=" h-auto mx-auto mb-4 animate-slide-down"
+          />
+        </Link>
       </div>
 
       {/* Form Container */}
@@ -76,7 +71,7 @@ const Form = () => {
                   htmlFor="name"
                   className="col-span-1 flex items-center md:text-[25px] text-[17px] font-[400] font-bebas"
                 >
-                 FULL NAME:
+                  FULL NAME:
                 </label>
                 <div className="col-span-2">
                   <input
@@ -109,8 +104,8 @@ const Form = () => {
                   />
                 </div>
 
-                 {/* PASSWORD field */}
-                 <label
+                {/* PASSWORD field */}
+                <label
                   htmlFor="password"
                   className="col-span-1 flex items-center md:text-[25px] text-[17px] font-[400] font-bebas"
                 >
@@ -148,117 +143,91 @@ const Form = () => {
                 </div>
               </div>
 
-
               {/* register button */}
               <div className="mt-8 w-full">
-              <div className="text-center">
-                <button
-                  onClick={handleSubmit}
-                  className="w-[91%] bg-[#FFB32C] text-white text-3xl font-medium font-bebas py-2 lg:py-4 rounded-2xl shadow-lg  hover:bg-[#ffd07f] transition-colors mb-7 "
-                >
-                  REGISTER
-                </button>
+                <div className="text-center">
+                  <button
+                    onClick={handleSubmit}
+                    className="w-[91%] bg-[#FFB32C] text-white text-3xl font-medium font-bebas py-2 lg:py-4 rounded-2xl shadow-lg  hover:bg-[#ffd07f] transition-colors mb-7 "
+                  >
+                    REGISTER
+                  </button>
+                </div>
               </div>
-            </div>
-
-
 
               {/* Role Selection */}
               <div className="w-full flex flex-col  mt-4 font-bebas">
-              <div className="md:col-span-2 flex flex-col  space-x-8 space-y-4">
-              <label className="inline-flex items-center">
-              <input
-                  type="checkbox"
-                  name="rememberMe"
-                  checked={formData.rememberMe}
-                  onChange={(e) =>
-                    setFormData((prev) => ({
-                      ...prev,
-                      rememberMe: e.target.checked, 
-                    }))
-                  }
-                  className="w-5 h-5 lg:w-6 lg:h-6 border-2 border-black appearance-none relative rounded-none checked:bg-transparent checked:before:content-[''] checked:before:absolute checked:before:top-[3px] checked:before:left-[3px] checked:before:w-[10px] checked:before:h-[10px] lg:checked:before:w-[14px] lg:checked:before:h-[14px] checked:before:bg-orange-300"
+                <div className="md:col-span-2 flex flex-col  space-x-8 space-y-4">
+                  <label className="inline-flex items-center">
+                    <input
+                      type="checkbox"
+                      name="rememberMe"
+                      checked={formData.rememberMe}
+                      onChange={(e) =>
+                        setFormData((prev) => ({
+                          ...prev,
+                          rememberMe: e.target.checked,
+                        }))
+                      }
+                      className="w-5 h-5 lg:w-6 lg:h-6 border-2 border-black appearance-none relative rounded-none checked:bg-transparent checked:before:content-[''] checked:before:absolute checked:before:top-[3px] checked:before:left-[3px] checked:before:w-[10px] checked:before:h-[10px] lg:checked:before:w-[14px] lg:checked:before:h-[14px] checked:before:bg-orange-300"
+                    />
+                    <span className="ml-2  font-medium md:text-lg text-[13px]">
+                      REMEMBER ME
+                    </span>
+                  </label>
 
-                />
-                      <span className="ml-2  font-medium md:text-lg text-[13px]">REMEMBER ME</span>
-                    </label>
-
-                    <label className="inline-flex items-center">
-                      <input
-                        type="checkbox"
-                        name="acceptTerms"
-                        value="mentor"
-                        checked={formData.acceptTerms}
-                        onChange={(e) =>
-                          setFormData((prev) => ({
-                            ...prev,
-                            acceptTerms: e.target.checked 
-                          }))
-                        }
-                        className="w-5 h-5 lg:w-6 lg:h-6 border-2 border-black appearance-none relative rounded-none checked:bg-transparent checked:before:content-[''] checked:before:absolute checked:before:top-[3px] checked:before:left-[3px] checked:before:w-[10px] checked:before:h-[10px] lg:checked:before:w-[14px] lg:checked:before:h-[14px] checked:before:bg-orange-300"
-                      />
-                      <span className="ml-2 md:text-lg text-[13px] font-medium">
-                        I do accept the <span className="underline">Terms and condition</span> of your site.
-                      </span>
-                    </label>
-                  </div>
+                  <label className="inline-flex items-center">
+                    <input
+                      type="checkbox"
+                      name="acceptTerms"
+                      value="mentor"
+                      checked={formData.acceptTerms}
+                      onChange={(e) =>
+                        setFormData((prev) => ({
+                          ...prev,
+                          acceptTerms: e.target.checked,
+                        }))
+                      }
+                      className="w-5 h-5 lg:w-6 lg:h-6 border-2 border-black appearance-none relative rounded-none checked:bg-transparent checked:before:content-[''] checked:before:absolute checked:before:top-[3px] checked:before:left-[3px] checked:before:w-[10px] checked:before:h-[10px] lg:checked:before:w-[14px] lg:checked:before:h-[14px] checked:before:bg-orange-300"
+                    />
+                    <span className="ml-2 md:text-lg text-[13px] font-medium">
+                      I do accept the{" "}
+                      <span className="underline">Terms and condition</span> of
+                      your site.
+                    </span>
+                  </label>
                 </div>
+              </div>
 
+              {/* Sign up with Selection */}
 
-                {/* Sign up with Selection */}
-
-                <div className="w-full flex flex-col  items-center">
-                  <h2 className="text-xl font-bebas md:text-2xl lg:text-3xl font-medium mb-5 mt-6">
+              <div className="w-full flex flex-col  items-center">
+                <h2 className="text-xl font-bebas md:text-2xl lg:text-3xl font-medium mb-5 mt-6">
                   Sign-up with:
-                  </h2>
-                  <div className="flex flex-row gap-[8%] justify-center w-full mb-5">
-                    <div className="flex flex-col items-center">
-                        <Image
-                        src={google}
-                        alt="GoldFish Ladder"
-                        width={40}
-                        height={40}
-                        className="w-[40px] h-[40px] max-w-3xl  mx-auto mb-4  "
-                      />
-                      <p className="text-sm  md:text-md lg:text-md  ">Google</p>
-                    </div>  
-                      
-                   
-                    <div>
-                      <Image
-                        src={insta}
-                        alt="GoldFish Ladder"
-                        width={40}
-                        height={40}
-                        className="w-[40px] h-[40px] max-w-3xl  mx-auto mb-4 animate-slide-down"
-                      />
-                      <p className="text-sm  md:text-md lg:text-md  ">Instagram</p>
-                    </div>
-                    <div>
-                      <Image
-                        src={lindedin}
-                        alt="GoldFish Ladder"
-                        width={40}
-                        height={40}
-                        className="w-[40px] h-[40px] max-w-3xl mx-auto mb-4 animate-slide-down"
-                      />                    
-                     <p className="text-sm md:text-md lg:text-md ">LinkedIn</p> 
-                    </div>
+                </h2>
+                <div className="flex flex-row gap-[8%] justify-center w-full mb-5">
+                  <div className="flex flex-col items-center">
+                    <Image
+                      src={google}
+                      alt="GoldFish Ladder"
+                      width={40}
+                      height={40}
+                      className="w-[40px] h-[40px] max-w-3xl  mx-auto mb-4  "
+                    />
+                    <p className="text-sm  md:text-md lg:text-md">Google</p>
                   </div>
-                  <p className="text-md font-bebas md:text-lg lg:text-xl font-medium ">Already a user?</p>
-                  <p className="text-md font-bebas md:text-lg lg:text-xl font-medium mb-12 text-[#854836]">login</p>
-                  
                 </div>
+                <p className="text-md font-bebas md:text-lg lg:text-xl font-medium ">
+                  Already a user?
+                </p>
+                <Link
+                  href={"/log-in"}
+                  className="text-md font-bebas md:text-lg lg:text-xl font-medium mb-12 text-[#854836]"
+                >
+                  login
+                </Link>
+              </div>
             </div>
-
-
-
-
-
-
-
-
-
           </div>
         </div>
       </div>
