03add733e2f01d684f26b65a144baceea95c7e7a
adnan <sirgeant10@gmail.com>
2025-06-02 23:35:35 +0600

    Remove header and footer from root layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,24 @@
-
-
-
 // app/layout.tsx
 import type { Metadata } from "next";
 import { Inter, Bebas_Neue, Josefin_Sans, Montserrat } from "next/font/google";
 import "./globals.css";
-import LayoutWrapper from "./LayoutWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
-const bebasNeue = Bebas_Neue({ 
+const bebasNeue = Bebas_Neue({
   weight: "400",
   subsets: ["latin"],
-  variable: "--font-bebas"
+  variable: "--font-bebas",
 });
-const josefinSans = Josefin_Sans({ 
+const josefinSans = Josefin_Sans({
   weight: ["400", "500", "600", "700"],
   subsets: ["latin"],
-  variable: "--font-josefin"
+  variable: "--font-josefin",
 });
-const montserrat = Montserrat({ 
+const montserrat = Montserrat({
   weight: ["400", "500", "600", "700"],
   subsets: ["latin"],
-  variable: "--font-montserrat"
+  variable: "--font-montserrat",
 });
-
-
 
 export const metadata: Metadata = {
   title: "GoldFish",
@@ -38,8 +32,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-    <body className={`${inter.className} ${bebasNeue.variable} ${josefinSans.variable} ${montserrat.variable} antialiased`}>
-        <LayoutWrapper>{children}</LayoutWrapper>
+      <body
+        className={`${inter.className} ${bebasNeue.variable} ${josefinSans.variable} ${montserrat.variable} antialiased`}
+      >
+        {children}
       </body>
     </html>
   );
