7df9ba160893cf7d31bb5054e3b7814fe320ca6f
adnan <sirgeant10@gmail.com>
2025-06-05 01:57:44 +0600

    Fix styling.

--- app/(root)/events/page.tsx
+++ app/(root)/events/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-"lucide-react"; // Ensure this file uses named exports
 import { motion } from "motion/react"; // Ensure this file uses named exports
 import EventCard from "@/components/cards/event-card";
 
@@ -72,60 +71,53 @@ export default function EventsPage() {
   ];
 
   return (
-    <div className="flex flex-col items-center mx-2 md:mx-9 my-9">
+    <div className="flex flex-col items-center">
       {/* Hero Section */}
       <section>
         <motion.div
-          className="max-w-[1047px] rounded-2xl px-2 md:px-14 py-[70px] bg-gradient-to-b from-[#ffb32c] to-[#b1662b] mt-[10px] sm:mx-[30px]"
+          className="max-w-5xl rounded-2xl px-2 md:px-14 py-[70px] bg-gradient-to-b from-[#ffb32c] to-[#b1662b] mt-4 mx-2 flex flex-col items-center gap-5 md:gap-10"
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true, amount: 0.2 }}
           transition={{ duration: 0.8 }}
         >
-          <div className="flex flex-col items-center gap-10">
-            {/* Title */}
-            <motion.div
-              className="w-full"
-              initial={{ opacity: 0, x: -50 }}
-              whileInView={{ opacity: 1, x: 0 }}
-              viewport={{ once: true, amount: 0.2 }}
-              transition={{ duration: 0.8 }}
-            >
-              <h1 className="text-5xl md:text-7xl font-bold text-white text-center leading-tight">
-                Explore Events
-              </h1>
-            </motion.div>
+          <motion.h1
+            initial={{ opacity: 0, x: -50 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            viewport={{ once: true, amount: 0.2 }}
+            transition={{ duration: 0.8 }}
+            className="w-full text-3xl md:text-7xl font-bold text-white text-center leading-tight"
+          >
+            “Events to Pitch, Learn, and Connect”
+          </motion.h1>
 
-            {/* Subtitle */}
-            <motion.div
-              className="mt-4 md:mt-11"
-              initial={{ opacity: 0, x: 50 }}
-              whileInView={{ opacity: 1, x: 0 }}
-              viewport={{ once: true, amount: 0.2 }}
-              transition={{ duration: 0.8, delay: 0.3 }}
-            >
-              <p className="md:text-2xl font-semibold text-white text-center leading-tight">
-                Join pitch nights, startup jams, hackathons, and virtual
-                meetups—all designed for young founders
-              </p>
-            </motion.div>
-          </div>
+          {/* Subtitle */}
+          <motion.p
+            initial={{ opacity: 0, x: 50 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            viewport={{ once: true, amount: 0.2 }}
+            transition={{ duration: 0.8, delay: 0.3 }}
+            className="text-xl md:text-2xl font-semibold text-white text-center leading-tight mt-4 md:mt-11"
+          >
+            Join pitch nights, startup jams, hackathons, and virtual meetups—all
+            designed for young founders
+          </motion.p>
         </motion.div>
       </section>
 
       {/* Currently Active Events */}
       <section className="mt-9 max-w-5xl">
         <motion.div
-          className="mt-[40px]"
+          className="mt-10 ml-2"
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true, amount: 0.2 }}
           transition={{ duration: 0.8 }}
         >
-          <h1 className="text-4xl md:text-5xl  font-bold mb-4 text-black lg:text-left">
+          <h1 className="text-4xl md:text-5xl font-bold mb-4">
             Currently Active
           </h1>
-          <p className="text-xl font-medium text-black lg:text-left">
+          <p className="text-xl font-medium">
             Discover exciting events happening now
           </p>
         </motion.div>
@@ -137,15 +129,16 @@ export default function EventsPage() {
         </div>
       </section>
 
-      <section className="mt-9 max-w-[1047px]">
+      {/* Explore Events */}
+      <section className="mt-9 max-w-5xl">
         <motion.div
-          className="flex flex-col w-full mt-10"
+          className="mt-10 ml-2"
           initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true, amount: 0.2 }}
           transition={{ duration: 0.8 }}
         >
-          <h1 className="text-3xl md:text-5xl font-bold mb-4">
+          <h1 className="text-4xl md:text-5xl font-bold mb-4">
             Explore Events
           </h1>
         </motion.div>
@@ -158,7 +151,7 @@ export default function EventsPage() {
       </section>
 
       {/* Call to Action */}
-      <section className="bg-primaryColor py-20 max-w-[1047px] rounded-xl mt-24 md:px-12">
+      <section className="bg-primaryColor py-20 px-2 max-w-5xl rounded-xl my-24 mx-2 md:px-12">
         <motion.div
           className="flex flex-col w-full w-max-[1047px]"
           initial={{ opacity: 0, y: 50 }}
@@ -169,18 +162,16 @@ export default function EventsPage() {
           <h1 className="text-4xl md:text-5xl font-bold mb-4 text-left">
             Next Generation Entrepreneurs
           </h1>
-          <p className="text-xl font-medium lg:text-left text-center">
-            Explore Events
-          </p>
+          <p className="text-xl font-medium">Explore Events</p>
         </motion.div>
 
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-3 w-full max-w-4xl mx-auto px-4">
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-3 w-full max-w-4xl mx-auto">
           {activeEvents.map((item) => (
             <EventCard key={item.id} card={item} />
           ))}
         </div>
 
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-3 w-full max-w-4xl mx-auto px-4">
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-3 w-full max-w-4xl mx-auto ">
           {exploreEvents.map((item) => (
             <EventCard key={item.id} card={item} />
           ))}
